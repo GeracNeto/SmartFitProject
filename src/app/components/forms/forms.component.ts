8a5fe1ceec023d203b992da53d15d99c0f5ac0e8
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-09-08 17:08:04 -0300

    implements FormBuilder

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
   public results = [];
+
+  public formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  public onSubmit() {
+    console.log(this.formGroup);
+  }
+
+  public onClean() {
+    this.formGroup.reset();
+  }
 }
